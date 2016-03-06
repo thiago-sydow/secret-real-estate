2b07007dd98a61b8e479f9d38c7741361184a41f
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 12:26:45 -0300

    add properties relationship test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  context 'associations' do
+    it { is_expected.to have_many :properties }
+  end
+
   context 'validations' do
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_presence_of :name }
