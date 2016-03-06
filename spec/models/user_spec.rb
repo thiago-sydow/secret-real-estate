b950f17bd31e463c2864ab2b78e738eacfceeb0e
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 11:45:02 -0300

    add user model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :role }
+  end
+
+  context 'authenticate user' do
+    let(:user) { create(:user) }
+
+    it { expect(user.valid_password?('password')).to be }
+    it { expect(user.valid_password?('falsepassword')).not_to be }
+  end
+
+  context 'callbacks' do
+    context '.after_initialize' do
+      let(:user) { build(:user) }
+
+      it { expect(user.guest?).to be_truthy }
+    end
+  end
+
+end
