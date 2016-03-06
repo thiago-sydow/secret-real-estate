7e2e83f1c702268b707cc3dcdd1f6d8802bb814a
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 17:46:32 -0300

    small refactor on rescues

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -6,24 +6,20 @@ module API
       default_error_formatter :json
       content_type :json, 'application/json'
 
-      rescue_from Pundit::NotAuthorizedError do |e|
-        opts = { error: "You have insuficient permissions." }
-        Rack::Response.new(opts.to_json, 403, {
-          'Content-Type' => "application/json",
-          'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Request-Method' => '*',
-        }).finish
-      end
-
       rescue_from :all do |e|
         eclass = e.class.to_s
+
         status = case
-        when eclass.match('RecordNotFound'), e.message.match(/unable to find/i).present?
-          404
-        else
-          (e.respond_to? :status) && e.status || 500
-        end
-        opts = { error: "#{e.message}" }
+          when eclass.match('RecordNotFound'), e.message.match(/unable to find/i).present?
+            404
+          when eclass.match('Pundit::NotAuthorizedError')
+            opts = { error: "You have insuficient permissions." }
+            403
+          else
+            (e.respond_to? :status) && e.status || 500
+          end
+
+        opts ||= { error: "#{e.message}" }
         opts[:trace] = e.backtrace[0,10] unless Rails.env.production?
         Rack::Response.new(opts.to_json, status, {
           'Content-Type' => "application/json",
@@ -32,12 +28,8 @@ module API
         }).finish
       end
 
-      # mount API::V1::Users
       mount API::V1::Properties
-
-      route :any, '*path' do
-        raise StandardError, "Unable to find endpoint"
-      end
+      mount API::V1::Users
     end
   end
 end
