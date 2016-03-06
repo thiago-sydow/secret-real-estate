6647bd1c4e6e922be6c1e41f90744fdf78c9e273
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 20:05:00 -0300

    dont allow blank values

--- app/controllers/api/v1/property_helpers.rb
+++ app/controllers/api/v1/property_helpers.rb
@@ -5,10 +5,10 @@ module API
 
       params :property_info do
         optional :property_info_attributes, type: Hash do
-          optional :bedrooms, type: Integer, desc: 'Number of bedrooms of the property'
-          optional :bathrooms, type: Integer, desc: 'Number of bathrooms of the property'
-          optional :car_spaces, type: Integer, desc: 'Number of car spaces of the property'
-          optional :square_footage, type: String, desc: 'Square footage of the property'
+          optional :bedrooms, type: Integer, allow_blank: false, desc: 'Number of bedrooms of the property'
+          optional :bathrooms, type: Integer, allow_blank: false, desc: 'Number of bathrooms of the property'
+          optional :car_spaces, type: Integer, allow_blank: false, desc: 'Number of car spaces of the property'
+          optional :square_footage, type: String, allow_blank: false, desc: 'Square footage of the property'
           at_least_one_of :bedrooms, :bathrooms, :car_spaces, :square_footage
         end
       end
