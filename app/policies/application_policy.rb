c6de75c464231db0aa263afeb851a2daa0cda58a
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 11:36:28 -0300

    add policies for user management

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,23 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  protected
+
+  def is_guest?
+    @user.guest?
+  end
+
+end
