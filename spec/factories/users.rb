bd61338752330cf1e05fe6de19487ccd85f1ef55
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 11:37:42 -0300

    add factory for user

---
+++ spec/factories/users.rb
@@ -0,0 +1,29 @@
+FactoryGirl.define do
+  factory :user do
+
+    trait :broker do
+      role :broker
+    end
+
+    trait :admin do
+      role :admin
+    end
+
+    trait :guest do
+      role :guest
+    end
+
+    sequence(:email) { |n| "user#{n}@example.com" }
+
+    password 'password'
+    password_confirmation 'password'
+    name 'Example User'
+
+    #Default user role is broker
+    guest
+
+    factory :user_admin, traits: [:admin]
+    factory :user_broker, traits: [:broker]
+  end
+
+end
