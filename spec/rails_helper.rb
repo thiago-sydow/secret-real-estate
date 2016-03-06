e776ac91426bf15d76238d09afe1f2e6ab2afdc6
Thiago von Sydow <thiagovs05@gmail.com>
2016-03-06 11:44:30 -0300

    add shoulda matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,3 +17,13 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
